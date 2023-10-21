78ff3a681d279dfaa95cc1c171d2e3cdd6ec2d7b
Lebogang <lebogangodeveloper@gmail.com>
2023-10-21 13:26:48 +0200

    Fix code to be PEP8 compliant

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This script starts a Flask web application for the HBNB project.
-It displays an HTML page for HBNB filters, listing states, cities, and amenities.
+Displays an HTML page for HBNB filters, listing states, cities, and amenities.
 """
 
 from flask import Flask, render_template
@@ -21,11 +21,12 @@ def close_storage(error):
 
 @app.route('/hbnb_filters', strict_slashes=False)
 def hbnb_filters():
-    """Displays the HBNB filters HTML page with states, cities, and amenities."""
+    """Displays the HBNB filters"""
     states = sorted(storage.all(State).values(), key=lambda x: x.name)
     cities = sorted(storage.all(City).values(), key=lambda x: x.name)
     amenities = sorted(storage.all(Amenity).values(), key=lambda x: x.name)
-    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
+    return render_template('10-hbnb_filters.html', states=states,
+                           cities=cities, amenities=amenities)
 
 
 if __name__ == '__main__':
